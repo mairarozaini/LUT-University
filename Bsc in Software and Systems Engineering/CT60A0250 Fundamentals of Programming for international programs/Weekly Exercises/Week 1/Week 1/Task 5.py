6d87ceb5a19ab9fa7168132598f20195b067a980
Humaira <87699494+mairarozaini@users.noreply.github.com>
2024-08-01 23:48:01 +0300

    Delete Bsc in Software and Systems Engineering/CT60A0250 Fundamentals of Programming for international programs/Weekly Exercises/Week 1/Week 1 directory

--- Bsc in Software and Systems Engineering/CT60A0250 Fundamentals of Programming for international programs/Weekly Exercises/Week 1/Week 1/Task 5.py
+++
@@ -1,6 +0,0 @@
-first_int = int(input("Give the first integer: "))
-sec_int = int(input("Give the first integer: "))
-quotient = int(first_int / sec_int)
-remainder = first_int % sec_int
-print(f"""Number {sec_int} goes to number {first_int} {quotient} times
-The remainder is {remainder}""")
